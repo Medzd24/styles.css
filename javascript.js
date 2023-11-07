74c081225a366396b8069c47c144e169402a452d
Medzd24 <150199255+Medzd24@users.noreply.github.com>
2023-11-08 00:03:18 +0100

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,28 @@
+const popupElement = document.getElementById("popup");
+    const countdownElement = document.getElementById("countdown");
+    const closeButton = document.getElementById("close-button");
+
+
+    setTimeout(function () {
+        
+        popupElement.style.display = "block";
+        
+        
+        let countdown = 8;
+        countdownElement.textContent = countdown;
+        
+        const countdownInterval = setInterval(function () {
+            countdown--;
+            countdownElement.textContent = countdown;
+            if (countdown <= 0) {
+                clearInterval(countdownInterval);
+                countdownElement.style.display = "none"; // J.*AJ 'D9/'/
+                closeButton.style.display = "block"; 
+            }
+        }, 1000);
+    }, 5000); // 'F*8'1 5 +H'FJ B(D 916 'D%9D'F
+    
+   
+    closeButton.addEventListener("click", function () {
+        popupElement.style.display = "none";
+    });
